0f03629f44dbd8306900aaaf53c855bcbf96f4aa
LumoHub <lumohub@hub.com>
2026-05-30 15:54:11 +0300

    Fix UI failing to load in Blade Ball Lobby

--- LumoHub.lua
+++ LumoHub.lua
@@ -102,18 +102,6 @@ local function LoadLumoHub(activeKey, authGui)
         local SpamParryEnabled = false
 
         local VirtualInputManager = game:GetService("VirtualInputManager")
-        local Balls = workspace:WaitForChild("Balls", 9e9)
-
-        local function VerifyBall(Ball)
-            if typeof(Ball) == "Instance" and Ball:IsA("BasePart") and Ball:IsDescendantOf(Balls) and Ball:GetAttribute("realBall") == true then
-                return true
-            end
-        end
-
-        local function IsTarget()
-            return (Player.Character and Player.Character:FindFirstChild("Highlight"))
-        end
-
         local function Parry()
             VirtualInputManager:SendMouseButtonEvent(0, 0, 0, true, game, 0)
             VirtualInputManager:SendMouseButtonEvent(0, 0, 0, false, game, 0)
@@ -121,34 +109,48 @@ local function LoadLumoHub(activeKey, authGui)
 
         local ParryDistance = 10 -- default distance multiplier
 
-        local function TrackBall(Ball)
-            if not VerifyBall(Ball) then return end
-            
-            local OldPosition = Ball.Position
-            local OldTick = tick()
+        task.spawn(function()
+            local Balls = workspace:WaitForChild("Balls", 9e9)
 
-            Ball:GetPropertyChangedSignal("Position"):Connect(function()
-                if not AutoParryEnabled then return end
-                if IsTarget() then
-                    local Distance = (Ball.Position - workspace.CurrentCamera.Focus.Position).Magnitude
-                    local Velocity = (OldPosition - Ball.Position).Magnitude
+            local function VerifyBall(Ball)
+                if typeof(Ball) == "Instance" and Ball:IsA("BasePart") and Ball:IsDescendantOf(Balls) and Ball:GetAttribute("realBall") == true then
+                    return true
+                end
+            end
 
-                    if Velocity > 0 and (Distance / Velocity) <= ParryDistance then
-                        Parry()
+            local function IsTarget()
+                return (Player.Character and Player.Character:FindFirstChild("Highlight"))
+            end
+
+            local function TrackBall(Ball)
+                if not VerifyBall(Ball) then return end
+                
+                local OldPosition = Ball.Position
+                local OldTick = tick()
+
+                Ball:GetPropertyChangedSignal("Position"):Connect(function()
+                    if not AutoParryEnabled then return end
+                    if IsTarget() then
+                        local Distance = (Ball.Position - workspace.CurrentCamera.Focus.Position).Magnitude
+                        local Velocity = (OldPosition - Ball.Position).Magnitude
+
+                        if Velocity > 0 and (Distance / Velocity) <= ParryDistance then
+                            Parry()
+                        end
                     end
-                end
 
-                if (tick() - OldTick >= 1/60) then
-                    OldTick = tick()
-                    OldPosition = Ball.Position
-                end
-            end)
-        end
+                    if (tick() - OldTick >= 1/60) then
+                        OldTick = tick()
+                        OldPosition = Ball.Position
+                    end
+                end)
+            end
 
-        Balls.ChildAdded:Connect(TrackBall)
-        for _, b in ipairs(Balls:GetChildren()) do
-            TrackBall(b)
-        end
+            Balls.ChildAdded:Connect(TrackBall)
+            for _, b in ipairs(Balls:GetChildren()) do
+                TrackBall(b)
+            end
+        end)
 
         CombatTab:CreateToggle({
             Name = "Auto Parry",
